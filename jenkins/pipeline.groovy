4b2eb469855a83e98f08f7dfbdb2e5a9e716cbad
Mykola Marzhan <mykola.marzhan@percona.com>
2018-05-31 20:10:44 +0300

    experimental rework archiving

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -120,6 +120,7 @@ pipeline {
                 sh '''
                     git reset --hard
                     git clean -xdf
+                    rm -rf sources/results
                     ./local/checkout
 
                     echo Build: \$(date -u "+%s")
