59bcd91cc99e461492ff7e22b3885b3c8bd38976
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-04-02 17:06:23 +0300

    Revert "JEN-1047 add timeout per stage and increase global timeout"
    This reverts commit 81eba3c8a595afb1821df63d171b9e8bd274da6b.

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -115,128 +115,122 @@ pipeline {
     options {
         skipDefaultCheckout()
         skipStagesAfterUnstable()
-        timeout(time: 3, unit: 'DAYS')
+        timeout(time: pipeline_timeout, unit: 'HOURS')
         buildDiscarder(logRotator(numToKeepStr: '200', artifactNumToKeepStr: '200'))
     }
     stages {
         stage('Build') {
             agent { label LABEL }
             steps {
-                timeout(time: 180, unit: 'MINUTES')  {
-                    retry(3) {
-                    script {
-                        currentBuild.displayName = "${BUILD_NUMBER} ${CMAKE_BUILD_TYPE}/${DOCKER_OS}"
-                    }
+                retry(3) {
+                script {
+                    currentBuild.displayName = "${BUILD_NUMBER} ${CMAKE_BUILD_TYPE}/${DOCKER_OS}"
+                }
 
-                    sh 'echo Prepare: \$(date -u "+%s")'
-                    echo 'Checking Percona Server branch version, JEN-913 prevent wrong version run'
-                    sh '''
-                        MY_BRANCH_BASE_MAJOR=8
-                        MY_BRANCH_BASE_MINOR=0
-                        RAW_VERSION_LINK=$(echo ${GIT_REPO%.git} | sed -e "s:github.com:raw.githubusercontent.com:g")
-                        wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                        source ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                        if [[ ${MYSQL_VERSION_MAJOR} -lt ${MY_BRANCH_BASE_MAJOR} ]] ; then
-                            echo "Are you trying to build wrong branch?"
-                            echo "You are trying to build ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR} instead of ${MY_BRANCH_BASE_MAJOR}.${MY_BRANCH_BASE_MINOR}!"
-                            rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                            exit 1
-                        fi
+                sh 'echo Prepare: \$(date -u "+%s")'
+                echo 'Checking Percona Server branch version, JEN-913 prevent wrong version run'
+                sh '''
+                    MY_BRANCH_BASE_MAJOR=8
+                    MY_BRANCH_BASE_MINOR=0
+                    RAW_VERSION_LINK=$(echo ${GIT_REPO%.git} | sed -e "s:github.com:raw.githubusercontent.com:g")
+                    wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                    source ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                    if [[ ${MYSQL_VERSION_MAJOR} -lt ${MY_BRANCH_BASE_MAJOR} ]] ; then
+                        echo "Are you trying to build wrong branch?"
+                        echo "You are trying to build ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR} instead of ${MY_BRANCH_BASE_MAJOR}.${MY_BRANCH_BASE_MINOR}!"
                         rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                    '''
-                    git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
-                    sh '''
-                        # sudo is needed for better node recovery after compilation failure
-                        # if building failed on compilation stage directory will have files owned by docker user
-                        sudo git reset --hard
-                        sudo git clean -xdf
-                        sudo rm -rf sources
-                        ./local/checkout
+                        exit 1
+                    fi
+                    rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                '''
+                git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
+                sh '''
+                    # sudo is needed for better node recovery after compilation failure
+                    # if building failed on compilation stage directory will have files owned by docker user
+                    sudo git reset --hard
+                    sudo git clean -xdf
+                    sudo rm -rf sources
+                    ./local/checkout
 
-                        echo Build: \$(date -u "+%s")
-                        sg docker -c "
-                            if [ \$(docker ps -q | wc -l) -ne 0 ]; then
-                                docker ps -q | xargs docker stop --time 1 || :
-                            fi
-                            ./docker/run-build ${DOCKER_OS}
-                        " 2>&1 | tee build.log
-
-                        echo Archive build: \$(date -u "+%s")
-                        sed -i -e '
-                            s^/tmp/ps/^sources/^;
-                            s^/tmp/results/^sources/^;
-                            s^/xz/src/build_lzma/^/third_party/xz-4.999.9beta/^;
-                        ' build.log
-                        gzip build.log
-
-                        if [[ -f build.log.gz ]]; then
-                            until aws s3 cp --no-progress --acl public-read build.log.gz s3://ps-build-cache/${BUILD_TAG}/build.log.gz; do
-                                sleep 5
-                            done
+                    echo Build: \$(date -u "+%s")
+                    sg docker -c "
+                        if [ \$(docker ps -q | wc -l) -ne 0 ]; then
+                            docker ps -q | xargs docker stop --time 1 || :
                         fi
+                        ./docker/run-build ${DOCKER_OS}
+                    " 2>&1 | tee build.log
 
-                        if [[ -f \$(ls sources/results/*.tar.gz | head -1) ]]; then
-                            until aws s3 cp --no-progress --acl public-read sources/results/*.tar.gz s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz; do
-                                sleep 5
-                            done
-                        else
-                            echo cannot find compiled archive
-                            exit 1
-                        fi
-                    '''
-                    }
+                    echo Archive build: \$(date -u "+%s")
+                    sed -i -e '
+                        s^/tmp/ps/^sources/^;
+                        s^/tmp/results/^sources/^;
+                        s^/xz/src/build_lzma/^/third_party/xz-4.999.9beta/^;
+                    ' build.log
+                    gzip build.log
+
+                    if [[ -f build.log.gz ]]; then
+                        until aws s3 cp --no-progress --acl public-read build.log.gz s3://ps-build-cache/${BUILD_TAG}/build.log.gz; do
+                            sleep 5
+                        done
+                    fi
+
+                    if [[ -f \$(ls sources/results/*.tar.gz | head -1) ]]; then
+                        until aws s3 cp --no-progress --acl public-read sources/results/*.tar.gz s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz; do
+                            sleep 5
+                        done
+                    else
+                        echo cannot find compiled archive
+                        exit 1
+                    fi
+                '''
                 }
             }
         }
         stage('Archive Build') {
             agent { label 'micro-amazon' }
             steps {
-                timeout(time: 60, unit: 'MINUTES')  {
-                    retry(3) {
-                        deleteDir()
-                        sh '''
-                            aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/build.log.gz ./build.log.gz
-                            gunzip build.log.gz
-                        '''
-                        warnings canComputeNew: false, canResolveRelativePaths: false, categoriesPattern: '', defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', messagesPattern: '', parserConfigurations: [[parserName: 'GNU C Compiler 4 (gcc)', pattern: 'build.log']], unHealthy: ''
-                    }
+                retry(3) {
+                deleteDir()
+                sh '''
+                    aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/build.log.gz ./build.log.gz
+                    gunzip build.log.gz
+                '''
+                warnings canComputeNew: false, canResolveRelativePaths: false, categoriesPattern: '', defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', messagesPattern: '', parserConfigurations: [[parserName: 'GNU C Compiler 4 (gcc)', pattern: 'build.log']], unHealthy: ''
                 }
             }
         }
         stage('Test') {
             agent { label LABEL }
             steps {
-                timeout(time: pipeline_timeout, unit: 'MINUTES')  {
-                    retry(3) {
-                        git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
-                        withCredentials([string(credentialsId: 'MTR_VAULT_TOKEN', variable: 'MTR_VAULT_TOKEN')]) {
-                            sh '''
-                                sudo git reset --hard
-                                sudo git clean -xdf
-                                rm -rf sources/results
-                                sudo git -C sources reset --hard || :
-                                sudo git -C sources clean -xdf   || :
+                retry(3) {
+                git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
+                    withCredentials([string(credentialsId: 'MTR_VAULT_TOKEN', variable: 'MTR_VAULT_TOKEN')]) {
+                        sh '''
+                            sudo git reset --hard
+                            sudo git clean -xdf
+                            rm -rf sources/results
+                            sudo git -C sources reset --hard || :
+                            sudo git -C sources clean -xdf   || :
 
-                                until aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz ./sources/results/binary.tar.gz; do
-                                    sleep 5
-                                done
+                            until aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz ./sources/results/binary.tar.gz; do
+                                sleep 5
+                            done
 
-                                echo Test: \$(date -u "+%s")
-                                sg docker -c "
-                                    if [ \$(docker ps -q | wc -l) -ne 0 ]; then
-                                        docker ps -q | xargs docker stop --time 1 || :
-                                    fi
-                                    ulimit -a
-                                    ./docker/run-test ${DOCKER_OS}
-                                "
+                            echo Test: \$(date -u "+%s")
+                            sg docker -c "
+                                if [ \$(docker ps -q | wc -l) -ne 0 ]; then
+                                    docker ps -q | xargs docker stop --time 1 || :
+                                fi
+                                ulimit -a
+                                ./docker/run-test ${DOCKER_OS}
+                            "
 
-                                echo Archive test: \$(date -u "+%s")
-                                gzip sources/results/*.output
-                                until aws s3 sync --no-progress --acl public-read --exclude 'binary.tar.gz' ./sources/results/ s3://ps-build-cache/${BUILD_TAG}/; do
-                                    sleep 5
-                                done
-                            '''
-                        }
+                            echo Archive test: \$(date -u "+%s")
+                            gzip sources/results/*.output
+                            until aws s3 sync --no-progress --acl public-read --exclude 'binary.tar.gz' ./sources/results/ s3://ps-build-cache/${BUILD_TAG}/; do
+                                sleep 5
+                            done
+                        '''
                     }
                 }
             }
