5653aff3df23d636efdbd56794519dd43d4df406
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-10-04 13:41:03 +0300

    JEN-1151 add centos8 and remove rhel8 os

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -51,7 +51,7 @@ pipeline {
             name: 'TOKUBACKUP_BRANCH',
             trim: true)
         choice(
-            choices: 'centos:6\ncentos:7\nubuntu:xenial\nubuntu:bionic\nubuntu:disco\ndebian:stretch\ndebian:buster\nroboxes-rhel8',
+            choices: 'centos:6\ncentos:7\ncentos:8\nubuntu:xenial\nubuntu:bionic\nubuntu:disco\ndebian:stretch\ndebian:buster',
             description: 'OS version for compilation',
             name: 'DOCKER_OS')
         choice(
