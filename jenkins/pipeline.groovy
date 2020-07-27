e177a623ef91b1e486663fd5ff05d8872b306f99
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-07-27 15:20:02 +0300

    ENG-898 Remove debian jessie (5.6)
        * remove debian jessie from PS tests because end of life
          was on June 30, 2020

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -35,7 +35,7 @@ pipeline {
             name: 'TOKUBACKUP_BRANCH',
             trim: true)
         choice(
-            choices: 'centos:6\ncentos:7\ni386/centos:6\nubuntu:xenial\nubuntu:bionic\nubuntu:disco\ndebian:jessie\ndebian:stretch',
+            choices: 'centos:6\ncentos:7\ni386/centos:6\nubuntu:xenial\nubuntu:bionic\nubuntu:disco\ndebian:stretch',
             description: 'OS version for compilation',
             name: 'DOCKER_OS')
         choice(
