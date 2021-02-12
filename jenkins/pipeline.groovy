c5dee1f980915d668f7a1dfe7e3bc34d3b90b42a
Illia P <illia.pshonkin@percona.com>
2021-02-12 10:30:44 +0200

    ENG-969: Implement AWS ECR for our internal build images

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -124,61 +124,63 @@ pipeline {
                 script {
                     currentBuild.displayName = "${BUILD_NUMBER} ${CMAKE_BUILD_TYPE}/${DOCKER_OS}"
                 }
-
-                sh 'echo Prepare: \$(date -u "+%s")'
-                echo 'Checking Percona Server branch version, JEN-913 prevent wrong version run'
-                sh '''
-                    MY_BRANCH_BASE_MAJOR=5
-                    MY_BRANCH_BASE_MINOR=6
-                    RAW_VERSION_LINK=$(echo ${GIT_REPO%.git} | sed -e "s:github.com:raw.githubusercontent.com:g")
-                    wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                    source ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                    if [[ ${MYSQL_VERSION_MAJOR} -ne ${MY_BRANCH_BASE_MAJOR} || ${MYSQL_VERSION_MINOR} -ne ${MY_BRANCH_BASE_MINOR} ]] ; then
-                        echo "Are you trying to build wrong branch?"
-                        echo "You are trying to build ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR} instead of ${MY_BRANCH_BASE_MAJOR}.${MY_BRANCH_BASE_MINOR}!"
-                        rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                        exit 1
-                    fi
-                    rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                '''
-                git branch: '5.6', url: 'https://github.com/Percona-Lab/ps-build'
-                sh '''
-                    git reset --hard
-                    git clean -xdf
-                    sudo rm -rf sources
-                    ./local/checkout
-
-                    echo Build: \$(date -u "+%s")
-                    sg docker -c "
-                        if [ \$(docker ps -q | wc -l) -ne 0 ]; then
-                            docker ps -q | xargs docker stop --time 1 || :
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c17fe66e-0927-4a1d-a32c-7eea5f0d3838', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh 'echo Prepare: \$(date -u "+%s")'
+                    echo 'Checking Percona Server branch version, JEN-913 prevent wrong version run'
+                    sh '''
+                        MY_BRANCH_BASE_MAJOR=5
+                        MY_BRANCH_BASE_MINOR=6
+                        RAW_VERSION_LINK=$(echo ${GIT_REPO%.git} | sed -e "s:github.com:raw.githubusercontent.com:g")
+                        wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                        source ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                        if [[ ${MYSQL_VERSION_MAJOR} -ne ${MY_BRANCH_BASE_MAJOR} || ${MYSQL_VERSION_MINOR} -ne ${MY_BRANCH_BASE_MINOR} ]] ; then
+                            echo "Are you trying to build wrong branch?"
+                            echo "You are trying to build ${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR} instead of ${MY_BRANCH_BASE_MAJOR}.${MY_BRANCH_BASE_MINOR}!"
+                            rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                            exit 1
                         fi
-                        ./docker/run-build ${DOCKER_OS}
-                    " 2>&1 | tee build.log
+                        rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
+                    '''
+                    git branch: '5.6', url: 'https://github.com/Percona-Lab/ps-build'
+                    sh '''
+                        git reset --hard
+                        git clean -xdf
+                        sudo rm -rf sources
+                        ./local/checkout
 
-                    echo Archive build: \$(date -u "+%s")
-                    sed -i -e '
-                        s^/tmp/ps/^sources/^;
-                        s^/tmp/results/^sources/^;
-                        s^/xz/src/build_lzma/^/third_party/xz-4.999.9beta/^;
-                    ' build.log
-                    gzip build.log
+                        echo Build: \$(date -u "+%s")
+                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
+                        sg docker -c "
+                            if [ \$(docker ps -q | wc -l) -ne 0 ]; then
+                                docker ps -q | xargs docker stop --time 1 || :
+                            fi
+                            ./docker/run-build ${DOCKER_OS}
+                        " 2>&1 | tee build.log
 
-                    if [[ -f build.log.gz ]]; then
-                        until aws s3 cp --no-progress --acl public-read build.log.gz s3://ps-build-cache/${BUILD_TAG}/build.log.gz; do
-                            sleep 5
-                        done
-                    fi
+                        echo Archive build: \$(date -u "+%s")
+                        sed -i -e '
+                            s^/tmp/ps/^sources/^;
+                            s^/tmp/results/^sources/^;
+                            s^/xz/src/build_lzma/^/third_party/xz-4.999.9beta/^;
+                        ' build.log
+                        gzip build.log
 
-                    if [[ -f \$(ls sources/results/*.tar.gz | head -1) ]]; then
-                        until aws s3 cp --no-progress --acl public-read sources/results/*.tar.gz s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz; do
-                            sleep 5
-                        done
-                    else
-                        echo cannot find compiled archive
-                        exit 1
-                    fi
-                '''
+                        if [[ -f build.log.gz ]]; then
+                            until aws s3 cp --no-progress --acl public-read build.log.gz s3://ps-build-cache/${BUILD_TAG}/build.log.gz; do
+                                sleep 5
+                            done
+                        fi
+
+                        if [[ -f \$(ls sources/results/*.tar.gz | head -1) ]]; then
+                            until aws s3 cp --no-progress --acl public-read sources/results/*.tar.gz s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz; do
+                                sleep 5
+                            done
+                        else
+                            echo cannot find compiled archive
+                            exit 1
+                        fi
+                    '''
+                }
             }
         }
         stage('Archive Build') {
@@ -197,29 +199,32 @@ pipeline {
             options { retry(3) }
             agent { label LABEL }
             steps {
-                git branch: '5.6', url: 'https://github.com/Percona-Lab/ps-build'
-                sh '''
-                    git reset --hard
-                    git clean -xdf
-                    rm -rf sources/results
-                    until aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz ./sources/results/binary.tar.gz; do
-                        sleep 5
-                    done
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'c17fe66e-0927-4a1d-a32c-7eea5f0d3838', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {      
+                    git branch: '5.6', url: 'https://github.com/Percona-Lab/ps-build'
+                    sh '''
+                        git reset --hard
+                        git clean -xdf
+                        rm -rf sources/results
+                        until aws s3 cp --no-progress s3://ps-build-cache/${BUILD_TAG}/binary.tar.gz ./sources/results/binary.tar.gz; do
+                            sleep 5
+                        done
 
-                    echo Test: \$(date -u "+%s")
-                    sg docker -c "
-                        if [ \$(docker ps -q | wc -l) -ne 0 ]; then
-                            docker ps -q | xargs docker stop --time 1 || :
-                        fi
-                        ulimit -a
-                        ./docker/run-test ${DOCKER_OS}
-                    "
+                        echo Test: \$(date -u "+%s")
+                        aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin public.ecr.aws/e7j3v3n0
+                        sg docker -c "
+                            if [ \$(docker ps -q | wc -l) -ne 0 ]; then
+                                docker ps -q | xargs docker stop --time 1 || :
+                            fi
+                            ulimit -a
+                            ./docker/run-test ${DOCKER_OS}
+                        "
 
-                    echo Archive test: \$(date -u "+%s")
-                    until aws s3 sync --no-progress --acl public-read --exclude 'binary.tar.gz' ./sources/results/ s3://ps-build-cache/${BUILD_TAG}/; do
-                        sleep 5
-                    done
-                '''
+                        echo Archive test: \$(date -u "+%s")
+                        until aws s3 sync --no-progress --acl public-read --exclude 'binary.tar.gz' ./sources/results/ s3://ps-build-cache/${BUILD_TAG}/; do
+                            sleep 5
+                        done
+                    '''
+                }
             }
         }
         stage('Archive') {
