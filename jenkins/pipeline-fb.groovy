18e6637bf48e3ff1fd18113d4bdf80f81aa661a3
Evgeniy Patlan <evgeniy.patlan@percona.com>
2021-07-19 09:49:09 +0300

    ZENFS testing

--- jenkins/pipeline-fb.groovy
+++ jenkins/pipeline-fb.groovy
@@ -92,12 +92,7 @@ pipeline {
                                 MY_BRANCH_BASE_MAJOR=fb-mysql-8
                                 MY_BRANCH_BASE_MINOR=0.13
                                 RAW_VERSION_LINK=$(echo ${GIT_REPO%.git} | sed -e "s:github.com:raw.githubusercontent.com:g")
-                                REPLY=$(curl -Is ${RAW_VERSION_LINK}/${BRANCH}/MYSQL_VERSION | head -n 1 | awk '{print $2}')
-                                if [[ ${REPLY} != 200 ]]; then
-                                    wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                                else
-                                    wget ${RAW_VERSION_LINK}/${BRANCH}/MYSQL_VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
-                                fi
+                                wget ${RAW_VERSION_LINK}/${BRANCH}/VERSION -O ${WORKSPACE}/VERSION-${BUILD_NUMBER}
                                 source ${WORKSPACE}/VERSION-${BUILD_NUMBER}
                                 if [[ ${MYSQL_VERSION_MAJOR} -lt ${MY_BRANCH_BASE_MAJOR} ]] ; then
                                     echo "Are you trying to build wrong branch?"
