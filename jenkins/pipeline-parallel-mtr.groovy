a322c6a810784adc59e57f9dd94f55fceb6a69e3
Anderson Nogueira <anderson.nogueira@percona.com>
2025-06-17 12:21:31 +0200

    Fix CCACHE_MAXSIZE to depend on groovy file constant
    - Pass CCACHE_MAXSIZE from groovy constant to docker build script
    - Update docker/run-build to use passed value with fallback to 8G
    - Addresses review comment from @inikep

--- jenkins/pipeline-parallel-mtr.groovy
+++ jenkins/pipeline-parallel-mtr.groovy
@@ -236,7 +236,7 @@ void build(String SCRIPT) {
                     if [ \$(docker ps -q | wc -l) -ne 0 ]; then
                         docker ps -q | xargs docker stop --time 1 || :
                     fi
-                    eval USE_CCACHE=yes KEEP_BUILD=yes ${SCRIPT} ${DOCKER_OS} ${WORKSPACE}/${WORK_DIR}
+                    eval USE_CCACHE=yes CCACHE_MAXSIZE=${CCACHE_MAXSIZE} KEEP_BUILD=yes ${SCRIPT} ${DOCKER_OS} ${WORKSPACE}/${WORK_DIR}
                 " 2>&1 | tee build.log
 
                 echo Archive build log: \$(date -u "+%s")
