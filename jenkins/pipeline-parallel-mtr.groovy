0b46153e1901c49e62ab19ff908fa0af57cc1fd8
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2025-10-13 11:00:42 +0200

    PS-10081: Remove wrong `PIPELINE_TIMEOUT` for `--big-test` in `pipeline-parallel-mtr.groovy`

--- jenkins/pipeline-parallel-mtr.groovy
+++ jenkins/pipeline-parallel-mtr.groovy
@@ -565,14 +565,6 @@ if (params.ANALYZER_OPTS.contains('-DWITH_VALGRIND=ON')) {
     PIPELINE_TIMEOUT = 144
 }
 
-if ( ((params.ANALYZER_OPTS.contains('-DWITH_ASAN=ON')) &&
-     (params.ANALYZER_OPTS.contains('-DWITH_ASAN_SCOPE=ON')) &&
-     (params.ANALYZER_OPTS.contains('-DWITH_UBSAN=ON'))) ||
-     ((params.MTR_ARGS.contains('--big-test')) || (params.MTR_ARGS.contains('--only-big-test'))) ) {
-    LABEL = 'docker-32gb'
-    PIPELINE_TIMEOUT = 20
-     }
-
 @NonCPS
 def fetchSlackUserId(email) {
     try {
